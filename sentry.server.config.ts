721f6874e2ade758b8fc93d47c196b9de5af066e
Brian Hill <gamepointbk@gmail.com>
2024-05-19 19:48:34 -0400

    bento styling, recent projects, floating nav styling, sentry setup

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://fd37d504fbc6b8a8fe21f4c71244c32c@o4507285666856960.ingest.us.sentry.io/4507285671116800",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
