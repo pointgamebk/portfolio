d36a823a6cb0ee082c1a3ad67d8931dc56e6f4c6
Brian Hill <gamepointbk@gmail.com>
2024-05-21 20:01:04 -0400

    footer bug

--- .history/components/Footer_20240521195716.tsx
+++ .history/components/Footer_20240521195716.tsx
@@ -31,7 +31,7 @@ const Footer = () => {
           />
         </a>
       </div>
-      <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-y-5">
+      <div className="flex mt-16 md:flex-row flex-col justify-between items-center gap-3">
         <p className="md:text-base text-sm md:font-normal font-light">
           Copyright © 2024 Brian Hill
         </p>
@@ -43,7 +43,7 @@ const Footer = () => {
                 key={info.id}
                 className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
               >
-                <img src={info.img} alt="icons" width={25} height={25} />
+                <img src={info.img} alt="icons" width={20} height={20} />
               </div>
             </a>
           ))}
--- .history/components/Footer_20240521195805.tsx
+++ .history/components/Footer_20240521195805.tsx
@@ -43,7 +43,7 @@ const Footer = () => {
                 key={info.id}
                 className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
               >
-                <img src={info.img} alt="icons" width={25} height={25} />
+                <img src={info.img} alt="icons" width={20} height={20} />
               </div>
             </a>
           ))}
