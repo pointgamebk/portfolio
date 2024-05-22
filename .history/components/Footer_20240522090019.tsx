d86b7cfa1901c072c0572b3a2ca09b60b2bec90b
Brian Hill <gamepointbk@gmail.com>
2024-05-22 09:07:24 -0400

    footer links

--- .history/components/Footer_20240522090019.tsx
+++ .history/components/Footer_20240522090019.tsx
@@ -40,14 +40,14 @@ const Footer = () => {
 
         <div className="flex items-center md:gap-3 gap-6">
           {socialMedia.map((info) => (
-            <div
-              key={info.id}
-              className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
-            >
-              <Link href={info.url}>
-                <Image src={info.img} alt="icons" width={25} height={25} />
-              </Link>
-            </div>
+            <a href={info.url} key={info.id}>
+              <div
+                key={info.id}
+                className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
+              >
+                <img src={info.img} alt="icons" width={25} height={25} />
+              </div>
+            </a>
           ))}
         </div>
       </div>
