5a4fd148fb1cb41812b253e2c2f0c60fb8824c24
Brian Hill <gamepointbk@gmail.com>
2024-05-27 17:46:52 -0400

    styling and copy

--- .history/data/index_20240527174251.ts
+++ .history/data/index_20240527174251.ts
@@ -71,7 +71,7 @@ export const gridItems = [
   },
   {
     id: 7,
-    title: "Currently developing a niche-specific streaming platform...",
+    title: "Currently developing a niche-specific streaming platform",
     description: "Whats's Next?",
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
