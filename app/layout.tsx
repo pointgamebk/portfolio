2adef5e431a346f9d2b87d5c6e87fac70d064de5
Brian Hill <gamepointbk@gmail.com>
2024-08-20 17:09:09 -0400

    styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} bg-black-100 w-full`}>
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
